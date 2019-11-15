bb4ed03e65584e4eb09195bdfcb7c3c4cc886aa3
DATAJOAQUIN <46810087+DATAJOAQUIN@users.noreply.github.com>
2019-11-15 11:30:33 +0100

    Add files via upload

---
+++ libraryapp/book/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    titulo = models.CharField(max_length = 50)
+    imagen = models.ImageField()
+    autor = models.CharField(max_length = 30, default='anonymous')
